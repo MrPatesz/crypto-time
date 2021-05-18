b01ba03b2dae39261c574ba460786d6415fefd7d
Patrik Ángyán <58144458+MrPatesz@users.noreply.github.com>
2021-05-18 16:03:41 +0200

    Created services, moved components to folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CoinsComponent } from './coins/coins.component';
-import { LoginComponent } from './login/login.component';
+import { CoinsComponent } from './components/coins/coins.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
