2d819bd1fef1f437581402a94908ae96feae361d
Patrik Ángyán <58144458+MrPatesz@users.noreply.github.com>
2021-05-22 18:10:30 +0200

    only crypto currencies

--- src/app/components/coins/add-coin/add-coin.component.ts
+++ src/app/components/coins/add-coin/add-coin.component.ts
@@ -3,24 +3,26 @@ import { MockedCoinsService } from 'src/app/services/coins/mocked-coins.service'
 import { MatDialogRef } from '@angular/material/dialog';
 import { Coin } from 'src/app/models/coin';
 import { ICoinsService } from 'src/app/services/coins/interface-coins.service';
+import { ApiCoinsService } from 'src/app/services/coins/api-coins.service';
 
 @Component({
   selector: 'app-add-coin',
   templateUrl: './add-coin.component.html',
   styleUrls: ['./add-coin.component.scss'],
-  providers: [{ provide: ICoinsService, useClass: MockedCoinsService }],
+  providers: [{ provide: ICoinsService, useClass: MockedCoinsService }],//ApiCoinsService}],//
 })
 export class AddCoinComponent implements OnInit {
   coins: Coin[] = [];
   selectedCoinId: string = '';
 
   constructor(
-    private coinsService: MockedCoinsService,
+    private coinsService: ICoinsService,
     public dialogRef: MatDialogRef<string>
   ) {}
 
   ngOnInit(): void {
     this.coinsService.getCoins().subscribe((data: Coin[]) => {
+      data = data.filter((c) => c.type_is_crypto);
       this.coins = data.slice(0, 100);
       localStorage.setItem('mockedCoins', JSON.stringify(this.coins));
     });
