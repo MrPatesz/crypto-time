24f44d6cb655377eed2c73ad70715ccd43b30dc6
Patrik Ángyán <58144458+MrPatesz@users.noreply.github.com>
2021-06-01 14:25:24 +0200

    logs in automaticly on registration

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { IUserService } from '../../services/user/interface-user.service';
-import { MockedUserService } from '../../services/user/mocked-user.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  providers: [{ provide: IUserService, useClass: MockedUserService }],
 })
 export class LoginComponent implements OnInit {
   username: string = '';
@@ -21,22 +19,25 @@ export class LoginComponent implements OnInit {
 
   onSubmit(): void {
     this.loginInvalid = false;
-    this.registered = false;
 
     let userExists = this.userService.userExists(this.username);
 
     if (userExists) {
-      let loggedInAs = this.userService.login(this.username, this.password);
-
-      if (loggedInAs) {
-        localStorage.setItem('loggedInAs', loggedInAs);
-        this.router.navigate(['coins']);
-      } else {
-        this.loginInvalid = true;
-      }
+      this.login();
     } else {
       this.userService.register(this.username, this.password);
-      this.registered = true;
+      this.login();
+    }
+  }
+
+  private login() {
+    let loggedInAs = this.userService.login(this.username, this.password);
+
+    if (loggedInAs) {
+      localStorage.setItem('loggedInAs', loggedInAs);
+      this.router.navigate(['coins']);
+    } else {
+      this.loginInvalid = true;
     }
   }
 }
